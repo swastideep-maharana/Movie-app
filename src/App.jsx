b18e56b7e69d85d49545776ee8727e261e706be0
swastideep maharana <swastideep67maharana@gmail.com>
2025-06-08 14:02:47 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Search from "./components/Search";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
+  );
+}
+export default App;
