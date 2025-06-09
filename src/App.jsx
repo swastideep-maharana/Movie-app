9a643b9d7f7d00e72c7a6edc661dd721d0b10f9d
swastideep maharana <swastideep67maharana@gmail.com>
2025-06-09 11:42:08 +0530

    added apprite database for tranding movies to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Search from "./components/Search";
-import Spinner from "./components/spinner";
-import MovieCard from "./components/MovieCard";
+import Search from "./components/Search.jsx";
+
+import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
+import { getTrendingMovies, updateSearchCount } from "./appwrite.js";
+import Spinner from "./components/spinner.jsx";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
-  method: "get",
+  method: "GET",
   headers: {
     accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 };
 
-function App() {
+const App = () => {
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
 
-  const [errorMessage, setErrorMessage] = useState("");
-
   const [movieList, setMovieList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const [isLoding, setIsLoding] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query = "") => {
-    setIsLoding(true);
+    setIsLoading(true);
     setErrorMessage("");
+
     try {
       const endpoint = query
         ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
@@ -38,27 +42,47 @@ function App() {
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
-        throw new Error("failed to fetch movies");
+        throw new Error("Failed to fetch movies");
       }
+
       const data = await response.json();
 
-      if (data.response === "false") {
-        setErrorMessage(data.error || "failed to fetch movies");
+      if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
         setMovieList([]);
         return;
       }
+
       setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
-      console.error(`Error fetching movies : ${error}`);
+      console.error(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
     } finally {
-      setIsLoding(false);
+      setIsLoading(false);
+    }
+  };
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
     }
   };
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
   return (
     <main>
@@ -75,10 +99,25 @@ function App() {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <section className="all movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
 
-          {isLoding ? (
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading ? (
             <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
@@ -93,5 +132,6 @@ function App() {
       </div>
     </main>
   );
-}
+};
+
 export default App;
